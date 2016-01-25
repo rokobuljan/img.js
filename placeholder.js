4cf5b4e0447084be7a592b8e1854b57ab5cb4ddb
roxon <roko.cb@gmail.com>
2016-01-25 14:32:40 +0100

    Added data attribute support. Modified HTML example accordingly

--- placeholder.js
+++ placeholder.js
@@ -1,11 +1,12 @@
 (function placeholder() {
-	var img = document.getElementsByTagName('img');
+	window.URL = window.URL || window.webkitURL;
+	var img = document.querySelectorAll('img, [data-placeholder]');
 	if(img.length > 0) {
 		var	canvas = document.createElement('canvas'), // In memory canvas
 			ctx = canvas.getContext("2d");
 		for(var i=0; i<img.length; i++){
 			var im = img[i],
-				src = im.src;
+				src = im.src || im.dataset.placeholder;
 			if(src.match(/img.js/)){
 				src = decodeURIComponent( src );
 				var pt = src.split("img.js")[1].split("&")[0].split('/'),
@@ -13,8 +14,8 @@
 					size = {w:_size[0], h:(_size[1]||_size[0])},
 					txt = src.split("&text=")[1]||size.w+'×'+size.h,
 					fontSize = ~~(size.w / txt.length);
-				canvas.width=size.w;
-				canvas.height=size.h;
+				canvas.width = size.w;
+				canvas.height = size.h;
 				// BG
 				ctx.fillStyle = "#"+(pt[2]||"ddd"); 
 				ctx.fillRect(0, 0, size.w, size.h); 
@@ -22,11 +23,11 @@
 				ctx.textAlign = 'center';
 				ctx.textBaseline = 'middle';
 				ctx.font = fontSize+'pt Calibri';
-				ctx.fillStyle = "#"+(pt[3]||"aaa"); // Text style
+				ctx.fillStyle = pt[3] ? "#"+pt[3] : "rgba(255,255,255,0.7)"; // Text style
 				ctx.fillText(txt, size.w/2, size.h/2); // TEXT
-				im.src=canvas.toDataURL();
-				im.width=size.w;
-				im.height=size.h;
+				im.width = size.w;
+				im.height = size.h;
+				im.src = canvas.toDataURL();
 			}
 		}
 	}
